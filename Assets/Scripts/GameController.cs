a265fb40055b18f913ef2d839a24f891df2a0ac2
Ahmet E. Dag <adag21044@gmail.com>
2024-09-11 22:06:28 +0300

    Game Controller done

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(GameUI))]
+public class GameController : MonoBehaviour
+{
+    public  GameController Instance { get; private set; }
+
+    [SerializeField] private int knifeCount;
+
+    [Header("Knife Spawning")]
+    [SerializeField] private Vector2 knifeSpawnPosition;
+    [SerializeField] private GameObject knifeObject;
+
+    public GameUI gameUI { get; private set;}
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        gameUI = GetComponent<GameUI>();
+    }
+
+    private void Start()
+    {
+        gameUI.SetInitialDisplayedKnifeCount(knifeCount);
+        SpawnKnife();
+    }
+
+    public void OnSuccessfulKnifeHit()
+    {
+        if(knifeCount > 0)
+        {
+      
+            SpawnKnife();
+        }
+        else
+        {
+            StartGameOverSequence(true);
+        }   
+    }
+
+    private void SpawnKnife()
+    {
+        knifeCount--;
+        Instantiate(knifeObject, knifeSpawnPosition, Quaternion.identity);
+    }
+
+    public void StartGameOverSequence(bool win)
+    {
+        StartCoroutine("GameOverSequence", win);
+    }
+
+    private IEnumerator GameOverSequence(bool win)
+    {
+        if(win)
+        {
+            yield return new WaitForSecondsRealtime(0.3f);
+        }
+        else
+        {
+            gameUI.ShowRestartButton();
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject restartButton;
+
+    [Header("Knife Count Display")]
+    
+    [SerializeField]
+    private GameObject panelKnives;
+    
+    [SerializeField]
+    private GameObject iconKnife;
+
+    [SerializeField]
+    private Color usedKnifeColor;
+
+    public void ShowRestartButton()
+    {
+        restartButton.SetActive(true);
+    }
+
+    public void SetInitialDisplayedKnifeCount(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(iconKnife, panelKnives.transform);
+        }
+    }
+
+    private int KnifeIconIndexToChange = 0;
+    
+    public void DecrementDisplayedKnifeCount()
+    {
+        panelKnives.transform.GetChild(KnifeIconIndexToChange).GetComponent<Image>().color = usedKnifeColor;
+    }
+}
